7e82b4ed1fd48d17461640e550c27d4b29fe557a
Arzan101 <khanazzu994@gmail.com>
2021-10-09 20:06:53 +0530

    commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Checkout.css";
 
-function s() {
-    return (
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+          <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
         <div>
-            <p>"hello"</p>
+          <h3 className="checkout__title">Your Shopping Details</h3>
+          {/* Cart Item here */}
+          {/* Cart Item here */}
         </div>
-    )
+      </div>
+
+      <div className="checkout__right">
+        <h3>SubTotal (2 item):</h3>
+      </div>
+    </div>
+  );
 }
 
-export default s
+export default Checkout;
